bebc248c9aff0d3db8f0fd5a669fb40c5805e1a1
Peter Hu <peterhu4000@gmail.com>
2022-07-25 22:41:09 -0700

    register

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -10,6 +10,18 @@ class UserController{
                 res.json(err)
             })
     }
+
+    register = (req, res) => {
+        User.create(req.body)
+            .then(user =>{
+                res.json({msg: 'success!', user: user});
+            })
+            .catch(err =>{
+                res.json(err)
+            })
+    }
+
+
 }
 
 module.exports = new UserController();
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,4 +2,5 @@ const UserController = require("../controllers/user.controllers");
 
 module.exports = (app) =>{
     app.get("/api/users",UserController.getAllUsers);
+    app.post("/api/users/register",UserController.register);
 }
